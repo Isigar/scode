9a98c90784e861e7668be1c00e6554f564fd3e84
Isigar <info@rcore.cz>
2020-11-22 15:34:01 +0100

    fix markers with update, fix cam inter

--- client/test/test.lua
+++ client/test/test.lua
@@ -25,6 +25,26 @@ Citizen.CreateThread(function()
     marker.stopRender()
 end)
 
+function createMarker()
+    marker = rcore:createMarker();
+    marker.setPosition(vector3(1729.89,3313.33,40.22))
+    marker.render();
+    marker.on('enter',function()
+        dbg.info('On enter')
+    end)
+    marker.on('leave', function()
+        dbg.info('on leave')
+    end)
+    marker.setKeys({
+        keys['E'],
+    })
+    marker.on('key', function(pressed)
+        dbg.info('on key press %s',pressed)
+    end)
+    Citizen.Wait(20000)
+    marker.stopRender()
+end
+
 Citizen.CreateThread(function()
     dbg.info('Starting create text test!')
     text = create3DText('Initial text');
